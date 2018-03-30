d40f9d86bc9541c112f9644f1c35074580c7447e
calin-darie <calin_darie@yahoo.com>
2018-03-30 18:07:00 +0300

    #1- introducing router
    default route redirect to /words
    adding PageNotFoundComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router'
 
 import { AppComponent } from './app.component';
+import { WordsComponent } from './words/words.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
+const routes: Routes = [
+  {
+    path: 'words',
+    component: WordsComponent
+  },
+  {
+    path: '',
+    redirectTo: 'words',
+    pathMatch: 'full'
+  },  
+  { 
+    path: '**', 
+    component: PageNotFoundComponent 
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WordsComponent,
+    PageNotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
