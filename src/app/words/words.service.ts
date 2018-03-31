78ba1cbefad7c8964209d054325414fe3f7afc35
calin-darie <calin_darie@yahoo.com>
2018-03-31 23:42:57 +0300

    #1 - using RomanianTTS generated speech in dummy data

--- src/app/words/words.service.ts
+++ src/app/words/words.service.ts
@@ -12,17 +12,17 @@ export class WordsService {
         new Word({
           text: 'casă',
           imageUrl: 'https://houseme.azureedge.net/images/Icons/svg/House-Me-Icons-SingleHouse.svg',
-          soundUrl: 'https://firebasestorage.googleapis.com/v0/b/aha-dev-environment.appspot.com/o/cas%C4%83.webm?alt=media&token=5d6089e2-a157-46cc-85a0-4b89cb528a0b'
+          soundUrl: 'https://firebasestorage.googleapis.com/v0/b/aha-dev-environment.appspot.com/o/speech%2Fcas%C4%83.mp3?alt=media&token=b5a23b46-acd2-4ba0-9bc8-b318b6ff6056'
         }),
         new Word({
           text: 'pasă',
           imageUrl: 'http://coaching.worldrugby.org/images/keyfactors/passing.jpg',
-          soundUrl: 'https://firebasestorage.googleapis.com/v0/b/aha-dev-environment.appspot.com/o/cas%C4%83.webm?alt=media&token=5d6089e2-a157-46cc-85a0-4b89cb528a0b'
+          soundUrl: 'https://firebasestorage.googleapis.com/v0/b/aha-dev-environment.appspot.com/o/speech%2Fpas%C4%83.mp3?alt=media&token=37ffb616-8031-45ec-8b53-dd2affb3633d'
         }),
         new Word({
           text: 'masă',
           imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzG2F1onhyUN0d1K0blpYV-dATTJ67sGcYF9o22YIryzHdbb8wcg',
-          soundUrl: 'https://firebasestorage.googleapis.com/v0/b/aha-dev-environment.appspot.com/o/cas%C4%83.webm?alt=media&token=5d6089e2-a157-46cc-85a0-4b89cb528a0b'
+          soundUrl: 'https://firebasestorage.googleapis.com/v0/b/aha-dev-environment.appspot.com/o/speech%2Fmas%C4%83.mp3?alt=media&token=b42fc256-8e6d-47d7-a810-1de96b6e8be4'
         })
       ]);
     });
